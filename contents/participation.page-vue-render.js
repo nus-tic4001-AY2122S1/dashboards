8a32daecc414f05997442cfbae321149b3899510
damithc <damithc@github>
2021-09-16 11:06:43 +0800

    Site Update. [skip ci]

--- contents/participation.page-vue-render.js
+++ contents/participation.page-vue-render.js
@@ -368,6 +368,6 @@ with(this){return _c('span',{attrs:{"data-mb-slot-name":"_content"}},[_c('strong
 with(this){return _c('span',{attrs:{"data-mb-slot-name":"_content"}},[_c('strong',[_v("LumiNUS Quiz(zes)")]),_v(": percentage of correct answers: "),_c('code',{pre:true,attrs:{"class":"hljs inline no-lang"}},[_v("72%")]),_v(" (2/3 points) "),_c('span',{staticClass:"dimmed"},[_v("(excluding bonus questions, if any) (within early-submission window? No)")])])}
 },function anonymous(
 ) {
-with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 3.0.6")]),_v(", generated on Wed, 15 Sept 2021, 21:34:46 GMT+8]")])])])}
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 3.0.6")]),_v(", generated on Thu, 16 Sept 2021, 11:06:24 GMT+8]")])])])}
 }];
   
